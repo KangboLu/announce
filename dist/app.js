956483022966fb291e61777d4d4f586bae775258
Tyler Hayes <thetylerhayes@gmail.com>
2016-09-07 15:17:49 -0700

    Basic functionality

--- dist/app.js
+++ dist/app.js
@@ -56,6 +56,17 @@
 	      }
 	      var me = user;
 	      console.log('me', me);
+
+	      // show live text preview
+	      $('.announce--input--text-entry').on('keyup', function(e) {
+	        console.log(e);
+	        $('.header--text-preview').html(e.target.value);
+	        $('.announce--share--button').css('background-color', '#2cbf76');
+	        if (e.target.value.length === 0) {
+	          $('.header--text-preview').html("Halo @ Jason's NOW ⚡️");
+	          $('.announce--share--button').css('background-color', '#888');
+	        }
+	      });
 	    });
 	  });
 	});
