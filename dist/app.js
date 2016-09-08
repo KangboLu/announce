2e43d19ffc62edaf13173bb8140dadcc0983b31f
Tyler Hayes <thetylerhayes@gmail.com>
2016-09-07 21:10:46 -0700

    Hide Done button on keyboard

--- dist/app.js
+++ dist/app.js
@@ -50,6 +50,7 @@
 	  console.log('document ready');
 	  Bebo.onReady(function() {
 	    console.log('bebo ready');
+	    Bebo.UI.disableKeyboardDoneStrip();
 	    Bebo.User.get('me', function(err, user){
 	      if(err){ 
 	          return console.log('error retrieving user', err);
--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   console.log('document ready');
   Bebo.onReady(function() {
     console.log('bebo ready');
+    Bebo.UI.disableKeyboardDoneStrip();
     Bebo.User.get('me', function(err, user){
       if(err){ 
           return console.log('error retrieving user', err);
